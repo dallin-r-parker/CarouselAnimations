02d45ea0b654d7ece73faa046a44c893016a7f71
Dallin parker <dallin.r.parker@icloud.com>
2018-07-15 02:41:34 -0400

    added storybook with an example

---
+++ src/Placeholder/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Placeholder extends Component {
+  render() {
+    return (
+      <div>
+        I'm just a place holder
+        <button>click me</button>
+      </div>
+    )
+  }
+}
---
+++ src/Placeholder/stories/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Placeholder from '../';
+
+storiesOf('Placeholder', module)
+  .add('rendered', () => {
+    return <Placeholder />
+  })
